35f5017bec68f92c50ca40b8aa13ac54f7b0a4c1
RoR143 <71833454+RoR143@users.noreply.github.com>
2022-03-25 12:49:29 +0100

    Add files via upload

---
+++ DialogueTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+
+    public Dialogue dialogue;
+    public bool timerTrigger;
+    public int timerSeconds;
+    public static bool conversationOver = false;
+    public GameObject currentConver;
+    public string converType;
+    public void TriggerDialogue()
+    {
+        DialogueManager.timerIsUsed = timerTrigger;
+        DialogueManager.timeToNext = timerSeconds;
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+        DialogueManager.converType = converType;
+    }
+
+    private void Update()
+    {
+        if (conversationOver == true)
+        {
+            currentConver.SetActive(false);
+            conversationOver = false;
+        }
+    }
+}
